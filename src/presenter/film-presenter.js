1f6a0fe68ec6e990cd236d21fa0c9ee0e8493cde
Петр <kravchenko.pb.13@gmail.com>
2023-02-26 12:52:50 +0300

    Добавит возможность переиспользования

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -1,4 +1,4 @@
-import { render } from '../framework/render';
+import { render, remove, replace } from '../framework/render';
 import FilmCardView from '../view/film-card-view';
 import FilmPopupPresenter from './film-popup-presenter';
 
@@ -6,6 +6,10 @@ import FilmPopupPresenter from './film-popup-presenter';
 export default class FilmPresenter {
   #film = null;
   #filmsListContainerComponent = null;
+  #commentsByFilm = null;
+  #filmPopupPresenter = null;
+  #filmCardComponent = null;
+
 
   constructor({ filmsListContainerComponent }) {
     this.#filmsListContainerComponent = filmsListContainerComponent;
@@ -13,9 +17,34 @@ export default class FilmPresenter {
 
   init(film, filmsModel) {
     this.#film = film;
-    const commentsByFilm = filmsModel.comments.filter((comment) => film.comments.includes(comment.id));
-    const filmPopupPresenter = new FilmPopupPresenter({ popupContainer: document.body, film, commentsByFilm });
-    const filmCardComponent = new FilmCardView({ film, onFilmCardClick: () => filmPopupPresenter.renderPopup() });
-    render(filmCardComponent, this.#filmsListContainerComponent);
+
+    this.#commentsByFilm = filmsModel.comments.filter((comment) => film.comments.includes(comment.id));
+    this.#filmPopupPresenter = new FilmPopupPresenter({
+      popupContainer: document.body,
+      film,
+      commentsByFilm: this.#commentsByFilm,
+    });
+
+    const prevFilmCardComponent = this.#filmCardComponent;
+    this.#filmCardComponent = new FilmCardView({film,
+      onFilmCardClick: this.#handleFilmCardClick
+    });
+
+    if (prevFilmCardComponent === null) {
+      render(this.#filmCardComponent, this.#filmsListContainerComponent);
+      return;
+    }
+
+    if (this.#filmsListContainerComponent.contains(prevFilmCardComponent.element)) {
+      replace(this.#filmCardComponent, prevFilmCardComponent);
+    }
+
+    remove(prevFilmCardComponent);
   }
+
+  destroy() {
+    remove(this.#filmCardComponent);
+  }
+
+  #handleFilmCardClick = () => this.#filmPopupPresenter.renderPopup();
 }
